2b9b6f81bc2d07f0423b67868a33f4de42ccc192
Benja254 <31890234+Benja254@users.noreply.github.com>
2023-09-14 08:28:11 +0300

    vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-add_lunch_combo twrp_C0070WW-user
-add_lunch_combo twrp_C0070WW-userdebug
-add_lunch_combo twrp_C0070WW-eng
+COMMON_LUNCH_CHOICES twrp_C0070WW-user
+COMMON_LUNCH_CHOICES twrp_C0070WW-userdebug
+COMMON_LUNCH_CHOICES twrp_C0070WW-eng
